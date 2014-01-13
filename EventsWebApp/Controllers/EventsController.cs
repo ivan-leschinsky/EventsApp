6869e687d66776a0275715f46e67e5f318e70358
Ivan Leschinsky <vanopiano@gmail.com>
2014-01-13 04:33:27 +0300

    Localization in another Controller

--- EventsWebApp/Controllers/EventsController.cs
+++ EventsWebApp/Controllers/EventsController.cs
@@ -188,28 +188,6 @@ namespace EventsWebApp.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult ChangeCulture(string lang)
-        {
-            string returnUrl = Request.UrlReferrer.AbsolutePath;
-            List<string> cultures = new List<string>() { "ru", "en" };
-            if (!cultures.Contains(lang))
-            {
-                lang = "ru";
-            }
-            HttpCookie cookie = Request.Cookies["lang"];
-            if (cookie != null)
-                cookie.Value = lang;
-            else
-            {
-                cookie = new HttpCookie("lang");
-                cookie.HttpOnly = false;
-                cookie.Value = lang;
-                cookie.Expires = DateTime.Now.AddYears(1);
-            }
-            Response.Cookies.Add(cookie);
-            return Redirect(returnUrl);
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
